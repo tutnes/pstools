2635f4dd8001f451899d9fb1e5e33fbd2496d9a5
Tarjei Utnes <tutnes@gmail.com>
2017-09-21 13:26:31 +0200

    Added a sql monitor

---
+++ sql-query.ps1
@@ -0,0 +1,26 @@
+#This was inspired by
+#https://stackoverflow.com/questions/8423541/how-do-you-run-a-sql-server-query-from-powershell
+
+function Invoke-SQL {
+    param(
+        [string] $dataSource = ".\SQLEXPRESS",
+        [string] $database = "MasterData",
+        [string] $sqlCommand = $(throw "Please specify a query.")
+      )
+
+    $connectionString = "Data Source=$dataSource; " +
+            "Integrated Security=SSPI; " +
+            "Initial Catalog=$database"
+
+    $connection = new-object system.data.SqlClient.SQLConnection($connectionString)
+    $command = new-object system.data.sqlclient.sqlcommand($sqlCommand,$connection)
+    $connection.Open()
+
+    $adapter = New-Object System.Data.sqlclient.sqlDataAdapter $command
+    $dataset = New-Object System.Data.DataSet
+    $adapter.Fill($dataSet) | Out-Null
+
+    $connection.Close()
+    $dataSet.Tables
+
+}
